1fa02c1aa1b8224557cecfc723266f46721b4b1f
anselbrandt <ansel@anselbrandt.com>
2024-12-28 17:14:32 -0800

    Add multiple progress bars

--- main.py
+++ main.py
@@ -14,46 +14,61 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+tasks = [
+    {"id": 123, "name": "one-two-three"},
+    {"id": 456, "name": "four-five-six"},
+    {"id": 789, "name": "seven-eight-nine"},
+]
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(
     request: Request, response: Response, hx_request: Optional[str] = Header(None)
 ):
-    context = {"request": request}
+    context = {"request": request, "tasks": tasks}
     return templates.TemplateResponse("index.html", context)
 
 
-@app.post("/start")
+@app.post("/start/{id}")
 async def start(
-    request: Request, response: Response, hx_request: Optional[str] = Header(None)
+    request: Request,
+    response: Response,
+    id: str,
+    hx_request: Optional[str] = Header(None),
 ):
-    db.set("progress", 0)
-    context = {"request": request}
+    db.set(f"progress_{id}", 0)
+    context = {"request": request, "id": id}
     return templates.TemplateResponse("start.html", context)
 
 
-@app.get("/job/progress")
+@app.get("/job/progress/{id}")
 async def job(
-    request: Request, response: Response, hx_request: Optional[str] = Header(None)
+    request: Request,
+    response: Response,
+    id: str,
+    hx_request: Optional[str] = Header(None),
 ):
-    current = db.get("progress")
+    current = db.get(f"progress_{id}")
     progress = int(current)
     if progress < 100:
         progress = progress + 10
-        db.set("progress", progress)
-        context = {"request": request, "progress": progress}
+        db.set(f"progress_{id}", progress)
+        context = {"request": request, "progress": progress, "id": id}
         return templates.TemplateResponse("progress.html", context)
     if progress == 100:
-        context = {"request": request, "progress": progress}
+        context = {"request": request, "progress": progress, "id": id}
         response.headers["HX-Trigger"] = "done"
         return templates.TemplateResponse(
             "progress.html", context, headers=response.headers
         )
 
 
-@app.get("/job")
+@app.get("/job/{id}")
 async def job(
-    request: Request, response: Response, hx_request: Optional[str] = Header(None)
+    request: Request,
+    response: Response,
+    id: str,
+    hx_request: Optional[str] = Header(None),
 ):
-    context = {"request": request}
+    context = {"request": request, "id": id}
     return templates.TemplateResponse("complete.html", context)
